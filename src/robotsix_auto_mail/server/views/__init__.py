282910166895838db5f5bbcb090ec3a2d921544b
robotsix-mill <mill@robotsix.local>
2026-06-26 20:23:02 +0000

    mill: Remove 4 dead backward-compat re-exports from server/views/__init__.py (20260626T194220Z-remove-4-dead-backward-compat-re-exports-ed5c)

--- src/robotsix_auto_mail/server/views/__init__.py
+++ src/robotsix_auto_mail/server/views/__init__.py
@@ -9,9 +9,6 @@ Split across focused submodules:
 from __future__ import annotations
 
 from robotsix_auto_mail.server.views.board import (
-    _batch_banner_html as _batch_banner_html,
-)
-from robotsix_auto_mail.server.views.board import (
     _build_board_content as _build_board_content,
 )
 from robotsix_auto_mail.server.views.board import (
@@ -23,27 +20,14 @@ from robotsix_auto_mail.server.views.board import (
 from robotsix_auto_mail.server.views.board import (
     _build_global_board_html as _build_global_board_html,
 )
-from robotsix_auto_mail.server.views.board import (
-    _gather_account_board_data as _gather_account_board_data,
-)
-from robotsix_auto_mail.server.views.board import (
-    _render_board_columns as _render_board_columns,
-)
-from robotsix_auto_mail.server.views.board import (
-    _render_board_page_shell as _render_board_page_shell,
-)
 from robotsix_auto_mail.server.views.detail import (
     _build_detail_html as _build_detail_html,
 )
 
 __all__ = [
-    "_batch_banner_html",
     "_build_board_content",
     "_build_board_html",
     "_build_detail_html",
     "_build_global_board_content",
     "_build_global_board_html",
-    "_gather_account_board_data",
-    "_render_board_columns",
-    "_render_board_page_shell",
 ]
